ac0fd3e8fcbd583a1f6954caa6b21323e4234300
Aldo Miralles <alditomiralles3@gmail.com>
2024-07-19 16:20:51 -0600

    changing to supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,11 +24,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_DATABASE || 'e-commerce',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
       ssl: process.env.POSTGRES_SSL === 'true',
